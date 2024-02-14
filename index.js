35354c7abcc771dd26bf2f3115454fa6dc197cfa
akroub <103196293+akroub11@users.noreply.github.com>
2024-02-14 09:36:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ client.interactions = new Collection();
 const commands = [];
 
 // Load commands
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client({checkUpdate:false}); 
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} is Ready For Working 24/7!`);
+})
+
+client.login(process.env.token);
 const commandFiles = fs
   .readdirSync("./src/commands")
   .filter((file) => file.endsWith(".js"));
